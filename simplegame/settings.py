f984523d13fb3f47f6b580d22c6ebbac6db0a05f
Anuj11041999 <anujchilwery@gmail.com>
2023-03-18 13:53:02 +0530

    second commit

--- simplegame/settings.py
+++ simplegame/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n#xv_*@#-qm+ab(d37r!n8fjwd(kl2m#_w&3=$f5rp_7+lhk#c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000']
 
 
 # Application definition
